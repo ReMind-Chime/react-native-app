d40f4b1b16a8a6cd6574e7f4ce9d7cd2d4fc9802
andyalv <andy.red02@gmail.com>
2025-05-06 12:19:45 -0600

    Remove unused imports in TabLayout component for cleaner code

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,12 @@
 import {Tabs} from 'expo-router';
 import React from 'react';
-import {Platform, View} from 'react-native';
+import {Platform} from 'react-native';
 
 import {HapticTab} from '@/components/HapticTab';
 import {IconSymbol} from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
-import {useColorScheme} from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-    const colorScheme = useColorScheme();
 
     const index = [
         {
