a7ea595c8e0c0d378db192548b5d68cf2eab74a5
andyalv <andy.red02@gmail.com>
2025-05-08 11:12:32 -0600

    Remove unused Text import from tab components

--- app/(tabs)/notes.tsx
+++ app/(tabs)/notes.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, View, Text} from "react-native";
+import {StyleSheet, View} from "react-native";
 import {ThemedText} from "@/components/ThemedText";
 
 export default function Page() {
