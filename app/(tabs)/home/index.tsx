4c503ca8c0c3382a79c90cdd3c77b7ef4f75151b
andyalv <andy.red02@gmail.com>
2025-05-09 03:07:17 -0600

    Add medication display with hardcoded data and refactor layout

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,29 +1,73 @@
-import { ThemedText } from "@/components/ThemedText";
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import { Link } from "expo-router";
-import { Pressable, SafeAreaView, View } from "react-native";
+import {ThemedText} from "@/components/ThemedText";
+import {View} from "react-native";
+import {MedCards} from "@/app/(tabs)/home/meds-cards";
+import {useEffect, useState} from "react";
+
+const array = [
+    {
+        med: 'Paracetamol',
+        dosage: '500mg',
+        nextDose: '12:00',
+    },
+    {
+        med: 'Ibuprofeno',
+        dosage: '400mg',
+        nextDose: '14:00',
+    },
+    {
+        med: 'Amoxicilina',
+        dosage: '250mg',
+        nextDose: '16:00',
+    },
+    {
+        med: 'Omeprazol',
+        dosage: '20mg',
+        nextDose: '18:00',
+    },
+    {
+        med: 'Loratadina',
+        dosage: '10mg',
+        nextDose: '20:00',
+    },
+    {
+        med: 'Metformina',
+        dosage: '500mg',
+        nextDose: '22:00',
+    },
+    {
+        med: 'Atorvastatina',
+        dosage: '10mg',
+        nextDose: '24:00',
+    },
+    {
+        med: 'Aspirina',
+        dosage: '100mg',
+        nextDose: '02:00',
+    },
+    {
+        med: 'Simvastatina',
+        dosage: '20mg',
+        nextDose: '04:00',
+    },
+    {
+        med: 'Clopidogrel',
+        dosage: '75mg',
+        nextDose: '06:00',
+    },
+]
 
 export default function Page() {
+    const [meds, setMeds] = useState(array);
+
     return (
-        <SafeAreaView className="flex-1 relative">
+        <View className="flex-1">
+
             {/* Contenido */}
-            <View className="flex-1 p-5">
+            <View className="flex-1 flex-cols gap-4 p-5">
                 <ThemedText type={'title'}>Medicina para hoy:</ThemedText>
+                <MedCards MedCardsArray={meds}/>
             </View>
 
-            {/* Botón flotante */}
-            <Link href="/(tabs)/home" asChild>
-                <Pressable
-                    className={`
-                        absolute bottom-[75] right-[15] z-10
-                        w-[48] h-[48]
-                        justify-center items-center
-                        bg-[#3B82F6] rounded-full
-                    `}
-                >
-                    <IconSymbol name="plus" color="black" size={28} />
-                </Pressable>
-            </Link>
-        </SafeAreaView>
+        </View>
     );
 }
