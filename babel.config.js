b24d0e679450499579a55cb37e07d92b3d057a9c
andyalv <andy.red02@gmail.com>
2025-06-04 07:14:14 -0600

    Implementation of WatermelonDB and plugins

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,17 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-  };
+    api.cache(true);
+    return {
+        presets: [
+            ["babel-preset-expo", {jsxImportSource: "nativewind"}],
+            "nativewind/babel",
+        ],
+        plugins: [
+            [
+                '@babel/plugin-proposal-decorators',
+                {
+                    legacy: true
+                }
+            ],
+        ]
+    };
 };
