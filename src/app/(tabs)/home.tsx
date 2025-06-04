b3e4f8b8c9f7d63a34b1372ed827741aeaba130c
andyalv <andy.red02@gmail.com>
2025-06-03 18:51:31 -0600

    Unversioned files from project structure changes

---
+++ src/app/(tabs)/home.tsx
@@ -0,0 +1,77 @@
+import {ThemedText} from "@/components/ThemedText";
+import {MedCards} from "@/components/screens/home/meds-cards";
+import {useState} from "react";
+import {MainView} from "@/components/MainView";
+import {StyleSheet} from "react-native";
+
+const array = [
+    {
+        med: 'Paracetamol',
+        dosage: '500mg',
+        nextDose: '12:00',
+    },
+    {
+        med: 'Ibuprofeno',
+        dosage: '400mg',
+        nextDose: '14:00',
+    },
+    {
+        med: 'Amoxicilina',
+        dosage: '250mg',
+        nextDose: '16:00',
+    },
+    {
+        med: 'Omeprazol',
+        dosage: '20mg',
+        nextDose: '18:00',
+    },
+    {
+        med: 'Loratadina',
+        dosage: '10mg',
+        nextDose: '20:00',
+    },
+    {
+        med: 'Metformina',
+        dosage: '500mg',
+        nextDose: '22:00',
+    },
+    {
+        med: 'Atorvastatina',
+        dosage: '10mg',
+        nextDose: '24:00',
+    },
+    {
+        med: 'Aspirina',
+        dosage: '100mg',
+        nextDose: '02:00',
+    },
+    {
+        med: 'Simvastatina',
+        dosage: '20mg',
+        nextDose: '04:00',
+    },
+    {
+        med: 'Clopidogrel',
+        dosage: '75mg',
+        nextDose: '06:00',
+    },
+]
+
+export default function Page() {
+    const [meds, setMeds] = useState(array);
+
+    return (
+        <MainView className="flex-cols gap-4">
+            <ThemedText type={'title'}>Medicina para hoy:</ThemedText>
+            <MedCards MedCardsArray={meds}/>
+        </MainView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+});
---
+++ src/app/(tabs)/stats.tsx
@@ -0,0 +1,18 @@
+import {StyleSheet, View} from "react-native";
+import {ThemedText} from "@/components/ThemedText";
+
+export default function Page() {
+  return (
+      <View style={styles.container}>
+        <ThemedText>Stats Screen</ThemedText>
+      </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+});
