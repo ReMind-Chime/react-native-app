631b1e91679c0b6f41bb6dc0fd07fd670ab17c29
andyalv <andy.red02@gmail.com>
2025-06-05 00:21:10 -0600

    Enhance Notes functionality by adding content field and refactoring NotesList component

--- src/app/(tabs)/notes.tsx
+++ src/app/(tabs)/notes.tsx
@@ -1,21 +1,60 @@
 import {StyleSheet, View} from "react-native";
 import {AddButton, NotesList} from "@/components/screens/notes";
+import {useState} from "react";
 
 
 const exampleNotes = [
-    { id: "1", title: "La Amoxicilina se debe ingerir con agua Natural", date: "03/mar", time: "10:00 AM" },
-    { id: "2", title: "El Ketorolaco no se ingiere, se pone bajo la lengua", date: "03/mar", time: "10:15 AM" },
-    { id: "3", title: "Las ampolletas de Vitamina se deben mezclar con jugo y beberse rápido", date: "03/mar", time: "12:00 PM" },
-    { id: "4", title: "Las ampolletas de Vitamina se deben mezclar con jugo y beberse rápido", date: "03/mar", time: "12:00 PM" },
-    { id: "5", title: "Las ampolletas de Vitamina se deben mezclar con jugo y beberse rápido", date: "03/mar", time: "12:00 PM" },
-    { id: "6", title: "Las ampolletas de Vitamina se deben mezclar con jugo y beberse rápido", date: "03/mar", time: "12:00 PM" },
+    {
+        id: "1",
+        title: "La Amoxicilina se debe ingerir con agua Natural",
+        content: "CContenido de ejemplo para la nota 1. Aquí puedes agregar más detalles sobre la nota.Contenido de ejemplo para la nota 1. Aquí puedes agregar más detalles sobre la nota.ontenido de ejemplo para la nota 1. Aquí puedes agregar más detalles sobre la nota.",
+        date: "03/mar",
+        time: "10:00 AM"
+    },
+    {
+        id: "2",
+        title: "El Ketorolaco no se ingiere, se pone bajo la lengua",
+        content: "Contenido de ejemplo para la nota 1. Aquí puedes agregar más detalles sobre la nota.",
+        date: "03/mar",
+        time: "10:15 AM"
+    },
+    {
+        id: "3",
+        title: "Las ampolletas de Vitamina se deben mezclar con jugo y beberse rápido",
+        content: "Contenido de ejemplo para la nota 1. Aquí puedes agregar más detalles sobre la nota.",
+        date: "03/mar",
+        time: "12:00 PM"
+    },
+    {
+        id: "4",
+        title: "Las ampolletas de Vitamina se deben mezclar con jugo y beberse rápido",
+        content: "Contenido de ejemplo para la nota 1. Aquí puedes agregar más detalles sobre la nota.",
+        date: "03/mar",
+        time: "12:00 PM"
+    },
+    {
+        id: "5",
+        title: "Las ampolletas de Vitamina se deben mezclar con jugo y beberse rápido",
+        content: "Contenido de ejemplo para la nota 1. Aquí puedes agregar más detalles sobre la nota.",
+        date: "03/mar",
+        time: "12:00 PM"
+    },
+    {
+        id: "6",
+        title: "Las ampolletas de Vitamina se deben mezclar con jugo y beberse rápido",
+        content: "Contenido de ejemplo para la nota 1. Aquí puedes agregar más detalles sobre la nota.",
+        date: "03/mar",
+        time: "12:00 PM"
+    },
 ];
 
-export default function Page() {
+export default function NotesPage() {
+    const [notes, setNotes] = useState(null)
+
     return (
         <View style={styles.container}>
 
-            <NotesList notes={exampleNotes} />
+            <NotesList notes={exampleNotes}/>
 
             <View style={styles.addButtonWrapper}>
                 <AddButton href={'/(modal)/notesform'}/>
--- src/components/screens/notes/NotesList.tsx
+++ src/components/screens/notes/NotesList.tsx
@@ -1,88 +1,93 @@
-import { StyleSheet, View, FlatList } from "react-native";
-import { ThemedText } from "@/components/ThemedText";
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import { TouchableOpacity } from "react-native";
-import { useRouter } from "expo-router";
+import {StyleSheet, View, FlatList} from "react-native";
+import {ThemedText} from "@/components/ThemedText";
+import {IconSymbol} from "@/components/ui/IconSymbol";
+import {TouchableOpacity} from "react-native";
+import {router} from "expo-router";
 
 interface Note {
-  id: string
-  title: string
-  time: string
-  date: string
+    id: string
+    title: string
+    content: string
+    time: string
+    date: string
 }
 
 interface NoteListProps {
-  notes: Note[]
+    notes: Note[]
 }
 
-export function NotesList({ notes }: NoteListProps) {
-  const router = useRouter();
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={notes}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={styles.noteCard}>
-            <ThemedText style={styles.title}>{item.title}</ThemedText>
-            <ThemedText style={styles.dateTime}>{item.date} - {item.time}</ThemedText>
+export function NoteComponent({note}: { note: Note }) {
+    return (
+        <View style={styles.noteCard}>
+            <ThemedText style={{fontSize: 18, fontWeight: 'bold'}}>{note.title}</ThemedText>
+            <ThemedText className={'text-[16px] mt-3'} numberOfLines={4}>{note.content}</ThemedText>
 
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity onPress={() => console.log(`Eliminar ${item.id}`)} style={styles.button}>
-                <IconSymbol name="trash" size={24} color="#d32f2f" />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => router.push({ pathname: "../notesform", params: { title: item.title, date: item.date, time: item.time } })} style={styles.button}>
-                <IconSymbol name="pencil" size={24} color="#1976d2" />
-              </TouchableOpacity>
+            <View className={'flex-row justify-between items-center mt-5'}>
+                {/* Fecha y hora de la nota */}
+                <ThemedText
+                    className={'font-bold text-[16px]'}
+                    style={{color: "#555"}}
+                >
+                    {note.date} - {note.time}
+                </ThemedText>
+
+                <View className={'flex-row'}>
+                    {/* Eliminar nota */}
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => console.log(`Eliminar ${note.id}`)}
+                    >
+                        <IconSymbol name="trash" size={24} color="#d32f2f"/>
+                    </TouchableOpacity>
+
+                    {/* Editar nota */}
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => router.push({
+                            pathname: "/notesform",
+                            params: {title: note.title, date: note.date, time: note.time}
+                        })}
+                    >
+                        <IconSymbol name="pencil" size={24} color="#1976d2"/>
+                    </TouchableOpacity>
+                </View>
+
             </View>
-          </View>
-        )}
-        showsVerticalScrollIndicator={false}
-      />
-    </View>
-  );
+        </View>
+    )
+}
+
+export function NotesList({notes}: NoteListProps) {
+    return (
+        <View className={'flex-1 px-8 mb-[9rem]'}>
+            <FlatList
+                data={notes}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => (
+                    <NoteComponent note={item}/>
+                )}
+                showsVerticalScrollIndicator={false}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 20,
-    marginBottom: 80, // Espacio para que el botón `+` no quede tapado
-  },
-  noteCard: {
-    backgroundColor: "#ffffff",
-    padding: 17, // padding para la cajita (alargar o recortar)
-    marginVertical: 10,
-    borderRadius: 8,
-    borderWidth: 2,
-    borderColor: "#ccc",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 2,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  dateTime: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#555", // Color más apagado para fecha y hora
-    marginTop: 8,
-  },
-
-  buttonContainer: {
-    flexDirection: "row",
-    marginTop: 12,
-    gap: 10,
-    position: "absolute",
-    left: 262,
-    bottom: -4,
-  },
-  button: {
-    padding: 8,
-    borderRadius: 5,
-  },
+    noteCard: {
+        backgroundColor: "#ffffff",
+        padding: 17,
+        marginVertical: 10,
+        borderRadius: 8,
+        borderWidth: 2,
+        borderColor: "#ccc",
+        shadowColor: "#000",
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 2,
+    },
+    button: {
+        padding: 8,
+        borderRadius: 5,
+    },
 });
