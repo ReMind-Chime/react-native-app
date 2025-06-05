e5eeb128851b08d637314ade81b109f20c2e7e77
andyalv <andy.red02@gmail.com>
2025-06-05 04:03:40 -0600

    Rename Post class to Notes for clarity and consistency

--- src/db/models/Notes.ts
+++ src/db/models/Notes.ts
@@ -1,7 +1,7 @@
 import { Model } from '@nozbe/watermelondb'
 import {field} from "@nozbe/watermelondb/decorators";
 
-export default class Post extends Model {
+export default class Notes extends Model {
     static table = 'notes'
 
     @field('title') title!: string;
